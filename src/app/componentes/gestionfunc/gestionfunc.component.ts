b98126e8aa1d6c4d56b9ef73f34c7066e880c42d
Roger Alonso Parra Sotelo <rparra@shd.gov.co>
2019-12-19 15:24:51 -0500

    gestion usuarios

--- src/app/componentes/gestionfunc/gestionfunc.component.ts
+++ src/app/componentes/gestionfunc/gestionfunc.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { CiudadanoService } from 'src/app/servicios/ciudadano.service';
+import {es} from '../../config/Propiedades';
 
 @Component({
   selector: 'app-gestionfunc',
@@ -8,15 +12,43 @@ import { Component, OnInit } from '@angular/core';
 export class GestionfuncComponent implements OnInit {
 
   opccrear: boolean;
+  ocultarCrear: boolean;
+  formulario: FormGroup;
+  es: any;
+  
 
-  constructor() {
+
+
+
+  constructor(private router: Router , private formBuilder: FormBuilder, private ciudadServ: CiudadanoService) {
     this.opccrear = true;
+    this.ocultarCrear = true;
+    this.formulario = this.formBuilder.group({
+      tipoAccion: [1],
+      usuario: [],
+      fechaInicio: [],
+      fechaFin: []
+    });
+
+
   }
 
   ngOnInit() {
+    this.es = es;
+    this.ocultarCrear = true;
+    this.opccrear = true;
   }
+  
+ 
   ejecutaaccion() {
-
+    // alert (this.formulario.value.tipoPersona);
+    if (this.formulario.value.tipoAccion === 1) { // Crear
+      this.ocultarCrear=true;
+    } else {
+      this.ocultarCrear = false;
+    }
+    // alert ('ok ' + this.tipoPersonaNat);
   }
+
 
 }
